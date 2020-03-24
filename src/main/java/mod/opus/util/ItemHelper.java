e6ef2a6d2d9f642d84d807ddcae4bf455cf0311b
GloriousAlpaca <31967704+GloriousAlpaca@users.noreply.github.com>
2020-03-24 22:52:19 +0100

    Method for easily creating BlockItems from Blocks

---
+++ src/main/java/mod/opus/util/ItemHelper.java
@@ -0,0 +1,14 @@
+package mod.opus.util;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class ItemHelper {
+
+	
+	public static BlockItem ItemfromBlock(Block p) {
+		return (BlockItem) new BlockItem(p, (new Item.Properties()).group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(p.getRegistryName());
+	}
+}
