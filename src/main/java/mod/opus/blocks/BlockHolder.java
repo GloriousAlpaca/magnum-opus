2c6bd7de6bd6cee94f1b641ff2d036c1fa67f9f7
GloriousAlpaca <31967704+GloriousAlpaca@users.noreply.github.com>
2020-03-25 18:02:44 +0100

    Block Objectholder

--- src/main/java/mod/opus/blocks/BlockHolder.java
+++ src/main/java/mod/opus/blocks/BlockHolder.java
@@ -6,12 +6,12 @@ import net.minecraftforge.registries.ObjectHolder;
 @ObjectHolder("opus")
 public class BlockHolder {
 
-	@ObjectHolder("opus:sulfurore")
+	@ObjectHolder("sulfurore")
 	public static final Block sulfurore = null;
 	
-	@ObjectHolder("opus:cinnabarore")
+	@ObjectHolder("cinnabarore")
 	public static final Block cinnabarore = null;
 	
-	@ObjectHolder("opus:saltore")
+	@ObjectHolder("saltore")
 	public static final Block saltore = null;
 }
